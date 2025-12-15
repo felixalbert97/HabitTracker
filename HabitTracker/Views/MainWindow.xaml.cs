df7a0ef0c8748afa5374d14164f2a8f3b0663af3
felixalbert97 <felixalbert97@web.de>
2025-12-15 10:43:05 +0100

    Buttons implementiert, Listendarstellung der Aufgaben angepasst, Logik zum Öffnen des Fenster zur Aufgabenerstellung hinzugefügt

--- HabitTracker/Views/MainWindow.xaml.cs
+++ HabitTracker/Views/MainWindow.xaml.cs
@@ -14,10 +14,32 @@ namespace HabitTracker.Views
 {
     public partial class MainWindow
     {
+        private readonly MainViewModel _mainViewModel;
+
         public MainWindow()
         {
             InitializeComponent();
-            DataContext = new MainViewModel();
+            _mainViewModel = new MainViewModel();
+            DataContext = _mainViewModel;
+
+            _mainViewModel.TaskCreationRequested += OnTaskCreationRequested;
+        }
+
+        public void OnTaskCreationRequested(object? sender, TaskCreationRequestedEventArgs args)
+        {
+            var taskCreationViewModel = new TaskCreationViewModel(args.Repository, args.StartDate);
+            var taskCreationWindow = new TaskCreationWindow
+            {
+                Owner = this,
+                DataContext = taskCreationViewModel
+            };
+
+            var result = taskCreationWindow.ShowDialog();
+            if (result == true)
+            {
+                _mainViewModel.LoadTasks();
+            }
+
         }
     }
 }
